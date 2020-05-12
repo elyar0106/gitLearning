dabc6416fb21ab11ec30732f7f53767e976e7ac2
elyar <56004018+elyar0106@users.noreply.github.com>
2020-05-12 17:05:52 +0200

    Update practice.java

--- src/tests/practice.java
+++ src/tests/practice.java
@@ -3,5 +3,11 @@ package tests;
 public class practice {
     public static void main(String[] args) {
         System.out.println("practice");
+        
+        //hello today
+        //add extra code  to test  git pull
+            
+         
+        
     }
 }
